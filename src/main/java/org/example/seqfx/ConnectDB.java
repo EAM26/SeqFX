59aab9c5829560a0defe596095591f2d77c4ac27
EAM26 <emuller2607@gmail.com>
2024-11-04 12:45:07 +0100

    Add database with connection

---
+++ src/main/java/org/example/seqfx/ConnectDB.java
@@ -0,0 +1,36 @@
+package org.example.seqfx;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectDB {
+
+    private Connection connection;
+
+    public ConnectDB() {
+        String url = "jdbc:sqlite:mydb.db";
+        try {
+            connection = DriverManager.getConnection(url);
+            System.out.println("Connection Successful");
+        } catch(SQLException e) {
+            System.out.println("Error Connecting to Database");
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public void closeConnection() {
+        try {
+            if(connection != null) {
+                connection.close();
+                System.out.println("Connection Closed");
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
