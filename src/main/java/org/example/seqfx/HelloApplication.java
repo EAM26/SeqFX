367d9745ab35191c01acf120c3dd76e03f94ae5d
EAM26 <emuller2607@gmail.com>
2024-11-04 12:50:20 +0100

    Add create table functionality to HelloApplication

--- src/main/java/org/example/seqfx/HelloApplication.java
+++ src/main/java/org/example/seqfx/HelloApplication.java
@@ -6,6 +6,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class HelloApplication extends Application {
     @Override
@@ -18,7 +21,19 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        ConnectDB connectDB = new ConnectDB();
+        Connection connection = connectDB.getConnection();
 
+        if(connection != null) {
+            try {
+                Statement statement = connection.createStatement();
+                String createTable = "CREATE TABLE IF NOT EXISTS views (id INTEGER PRIMARY KEY, name TEXT)";
+                statement.executeUpdate(createTable);
+
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 //        launch();
     }
 }
