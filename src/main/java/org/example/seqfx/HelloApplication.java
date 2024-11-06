1a2dd65d25c000f46b9d41e0b1954ee57eeb40cc
EAM26 <emuller2607@gmail.com>
2024-11-06 13:57:49 +0100

    Make ConnectDB static and available for controllers and daos

--- src/main/java/org/example/seqfx/HelloApplication.java
+++ src/main/java/org/example/seqfx/HelloApplication.java
@@ -4,13 +4,19 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.seqfx.controller.SequenceController;
 import org.example.seqfx.util.ConnectDB;
 import org.example.seqfx.util.DatabaseInitializer;
 
 import java.io.IOException;
 import java.sql.Connection;
 
+
 public class HelloApplication extends Application {
+
+
+    public static ConnectDB connectDB;
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
@@ -21,11 +27,16 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
-        ConnectDB connectDB = new ConnectDB();
-        Connection connection = connectDB.getConnection();
-        DatabaseInitializer.initializer(connection);
+        connectDB = new ConnectDB();
+        DatabaseInitializer.initializer(connectDB.getConnection());
+        SequenceController seqCon = new SequenceController();
+        seqCon.createSequence("testSequence");
 
 
 //        launch();
+    }
+
+    public static Connection getConnection() {
+        return connectDB.getConnection();
     }
 }
